cf157758c62436fcf71f977a098d522275940a54
bisman-kaur <91104146+bisman-kaur@users.noreply.github.com>
2022-02-28 22:37:33 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var first_name = "Bisman";
+var last_name = "Kaur";
+
+result=first_name.concat(last_name);
+console.log(result);
+
+var a=10;
+var b=5;
+
+add=(a+b);
+subtract=(a-b);
+multiply=(a*b);
+divide=(a/b);
+
+console.log(add);
+console.log(subtract);
+console.log(multiply);
+console.log(divide);
